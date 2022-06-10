796a254d3b566b027d260528cc30b75c31b90ea1
V V <firluk@gmail.com>
2022-06-10 21:05:21 +0300

    facet type finished

--- facet.py
+++ facet.py
@@ -1,30 +1,76 @@
+from enum import Enum
+
 import numpy as np
 import cv2 as cv
 
 
 class Facet:
+    class Type(Enum):
+        FLAT = 1
+        TAB = 2
+        BLANK = 3
 
-    def __init__(self, strip_coordinates, corners):
-        # TODO
-        self.strip_coordinates = strip_coordinates
-        self.type = None  # flat, male, female  # TODO this after alignment and such
+    def __init__(self, strip_coordinates):
+        def determine_type():
+            epsilon = cv.arcLength(strip_coordinates, False) * 0.1
+            approx = cv.approxPolyDP(strip_coordinates, epsilon, False)
+            xy = np.squeeze(approx)
+            if xy.shape[0] == 2:
+                return Facet.Type.FLAT
+
+            theta = np.arctan2(xy[-1, 1] - xy[0, 1], xy[-1, 0] - xy[0, 0])
+            c, s = np.cos(theta), np.sin(theta)
+            xy = xy @ np.array([[c, -s], [s, c]])
+            xy -= xy[0, :]
+            x_r, y_r = xy[:, 0], xy[:, 1]
+            x_r_c, y_r_c = np.mean(x_r), np.mean(y_r)
+
+            if y_r_c > 0:
+                _facet_type = Facet.Type.TAB
+            elif y_r_c < 0:
+                _facet_type = Facet.Type.BLANK
+            else:
+                _facet_type = Facet.Type.FLAT
+            return _facet_type
+
+        facet_type = determine_type()
+
+        self.strip_coordinates = strip_coordinates  # TODO: debate whether to squeeze or not to squeeze
+        self.type = facet_type
+
+        # NOTE: moved to a function
         # self.strip_mask = None  # binary mask
         # self.strip_image = None  # pixel under mask
         # self.corners = None  # identifiers
+
         # self.metrics = None  # TODO: will be decided further down the dev cycle, stub for now
 
-    def strip_mask(self, cropped_mask=None, shape=None):
+    def strip_mask(self, cropped_mask):
+        """
+        Retrieve the binary mask of the strip, relative to supplied cropped mask
+        :param cropped_mask
+        :return: binary mask of the strip
+        """
         if cropped_mask:
             strip_mask = cv.polylines(np.zeros_like(cropped_mask), [self.strip_coordinates], False, 255, 0)
-        elif shape:
-            strip_mask = cv.polylines(np.zeros(shape), [self.strip_coordinates], False, 255, 0)
         else:
-            raise Exception('Provide either cropped mask/image or shape to create a strip mask')
+            raise ValueError('Provide either cropped mask/image or shape to create a strip mask')
 
         return strip_mask
 
-    def strip_image(self, cropped_image=None, shape=None):
-        strip_mask = self.strip_mask(cropped_image, shape)
-        strip_image = np.array(cropped_image, copy=True)
-        strip_mask[strip_mask == 0] = 0
+    def strip_image(self, cropped_image):
+        """
+        Returns an image under the strip mask.
+        :param cropped_image: Image to copy the strip from
+        :return: RGB image with only contour strip visible
+        """
+        strip_mask = self.strip_mask(cropped_image)
+        strip_image = np.zeros_like(cropped_image)
+        strip_image[strip_mask] = cropped_image[strip_mask]
         return strip_image
+
+    def corners(self):
+        """
+        :return: Corners of the facet - first and last coordinates
+        """
+        return self.strip_coordinates[0], self.strip_coordinates[-1]
--- piece.py
+++ piece.py
@@ -1,122 +1,133 @@
 import cv2 as cv
 import numpy as np
-from scipy.signal import find_peaks, savgol_filter
+from scipy.signal import find_peaks, savgol_filter, peak_prominences
 
 from facet import Facet
 
 SAVGOL_WINDOW_RATIO = 0.04
 
-PAD = 10
-
 
 class Piece:
     def __init__(self, full_mask, image, piece_id):
         _, _, stats, _ = cv.connectedComponentsWithStats(full_mask)
-        self.left = stats[1, cv.CC_STAT_LEFT]
-        self.top = stats[1, cv.CC_STAT_TOP]
-        self.width = stats[1, cv.CC_STAT_WIDTH]
-        self.height = stats[1, cv.CC_STAT_HEIGHT]
-        self.cropped_mask = np.zeros((self.height + PAD * 2, self.width + PAD * 2), dtype='uint8')
-        self.cropped_mask[PAD:self.height + PAD, PAD:self.width + PAD] \
-            = full_mask[self.top:self.top + self.height, self.left:self.left + self.width]  # cropped bitmap
-        self.cropped_image = np.zeros((self.height + PAD * 2, self.width + PAD * 2), dtype='uint8')
-        self.cropped_image[PAD:self.height + PAD, PAD:self.width + PAD] \
-            = image[self.top:self.top + self.height, self.left:self.left + self.width]  # cropped rgb image
-        self.cropped_image[self.cropped_mask == 0] = 0
+        left = stats[1, cv.CC_STAT_LEFT]
+        top = stats[1, cv.CC_STAT_TOP]
+        width = stats[1, cv.CC_STAT_WIDTH]
+        height = stats[1, cv.CC_STAT_HEIGHT]
+
+        cropped_mask = np.zeros((height, width), dtype='uint8')
+        cropped_mask[:height, 0:width] \
+            = full_mask[top:top + height, left:left + width]  # cropped bitmap
+
+        cropped_image = np.zeros((height, width, 3), dtype='uint8')
+        cropped_image[0:height, 0:width, :] \
+            = image[top:top + height, left:left + width, :]  # cropped rgb image
+        cropped_image[cropped_mask == 0] = 0
+
+        contours, _ = cv.findContours(cropped_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        contour = contours[0]
+
+        def retrieve_corners():
+            """
+            Finds and retrieves the 4 corners of the piece
+            :return: (corners, corners_indices) - corners - (x,y) coordinates relative to cropped image dimensions
+                                                - corner_indices - corresponding to coordinates indices in contour
+            """
+            (center_x, center_y), _ = cv.minEnclosingCircle(contour)
+
+            def cart2pol(_x, _y):
+                """
+                Turns cartesian coordinates to polar
+                :param _x: x coordinate
+                :param _y: y coordinate
+                :return: (rho, phi): rho - length of the vector, phi - angle of the vector in radians
+                """
+                _rho = np.sqrt(_x ** 2 + _y ** 2)
+                _phi = np.arctan2(_y, _x)
+                return _rho, _phi
+
+            x, y = contour[:, 0, 0], contour[:, 0, 1]
+            rho, phi = cart2pol(x - center_x, y - center_y)
+
+            argmin_rho = np.argmin(rho)
+            window_length = len(rho) * SAVGOL_WINDOW_RATIO
+            window_length = np.ceil(window_length) // 2 * 2 + 1  # closest odd
+            window_length = window_length.astype(int)
+            rho, phi = np.append(np.roll(rho, -argmin_rho), rho[argmin_rho]), np.append(np.roll(phi, -argmin_rho),
+                                                                                        phi[argmin_rho])
+
+            smoothed_rho = savgol_filter(rho, window_length, 3)
+            peaks, _ = find_peaks(smoothed_rho, height=0)
+
+            window_length = int(cv.arcLength(contour, True) * 0.01 / 2)
+            prominences = peak_prominences(smoothed_rho, peaks, window_length)[0]
+
+            four_highest_prominence = np.argsort(prominences)[-4:]  # lowest 4
+            four_highest_prominence = np.sort(four_highest_prominence)  # sort ascending
+            four_highest_prominence = peaks[four_highest_prominence]  # contour indices
+            four_highest_prominence = np.mod(four_highest_prominence + argmin_rho,
+                                             x.shape)  # correct back shift
+            four_highest_prominence = np.sort(four_highest_prominence)  # sort ascending
+
+            four_four_highest_prominence_xy_coordinates = x[four_highest_prominence], y[four_highest_prominence]
+            return four_four_highest_prominence_xy_coordinates, four_highest_prominence
+
+        corners, corners_indices = retrieve_corners()
+
+        def create_facets():
+            lst = []  # list of Facet objects
+
+            contour_split = np.split(contour, corners_indices)
+            contour_split[0] = np.vstack((contour_split[-1], contour_split[0]))
+
+            for i in range(4):
+                strip_coordinates = contour_split[i]
+                # # visual debugging
+                from matplotlib import pyplot as plt
+                plt.imshow(cv.polylines(np.zeros_like(cropped_mask), [strip_coordinates], False, 255, 3))
+                plt.show()
+                lst.append(Facet(strip_coordinates))
+
+            return lst
+
+        facets = create_facets()
+
+        center = np.mean(corners)
+
+        # fields
         self.id = piece_id
+        self.left = left
+        self.top = top
+        self.cropped_mask = cropped_mask
+        self.cropped_image = cropped_image
+        self.contour = contour
+        self.corners = corners
+        self.center = center
+        self.facets = facets
 
-        contours, _ = cv.findContours(self.cropped_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-        self.contour = contours[0]
-        (center_x, center_y), _ = cv.minEnclosingCircle(self.contour)
-
-        # from matplotlib import pyplot as plt
-        # plt.plot(self.cropped_mask)
-        # plt.show()
-        # contour_mask = cv.drawContours(np.zeros_like(self.cropped_mask), contours, -1, 255, 0)
-        # plt.plot(contour_mask)
-        # plt.show()
-
-        def cart2pol(_x, _y):
-            """
-            Turns cartesian coordinates to polar
-            :param _x: x coordinate
-            :param _y: y coordinate
-            :return: (rho, phi): rho - length of the vector, phi - angle of the vector in radians
-            """
-            _rho = np.sqrt(_x ** 2 + _y ** 2)
-            _phi = np.arctan2(_y, _x)
-            return _rho, _phi
-
-        def pol2cart(_rho, _phi):
-            """
-            Turns polar coordinates to cartesian
-            :param _rho: length of the vector
-            :param _phi:  angle of the vector in radians
-            :return: (x,y) : x coordinate, y: y coordinate
-            """
-            _x = _rho * np.cos(_phi)
-            _y = _rho * np.sin(_phi)
-            return _x, _y
-
-        # # polygon approximation method for finding peaks
-        # epsilon = 0.01 * cv.arcLength(contour, True)
-        # approx = cv.approxPolyDP(contour, epsilon, True)
-        # x, y = approx[:, 0, 0], approx[:, 0, 1]
-
-        x, y = self.contour[:, 0, 0], self.contour[:, 0, 1]
-        rho, phi = cart2pol(x - center_x, y - center_y)
-
-        argmin_rho = np.argmin(rho)
-        window_length = len(rho) * SAVGOL_WINDOW_RATIO
-        window_length = np.ceil(window_length) // 2 * 2 + 1  # closest odd
-        window_length = window_length.astype(int)
-        rho, phi = np.append(np.roll(rho, -argmin_rho), rho[argmin_rho]), np.append(np.roll(phi, -argmin_rho),
-                                                                                    phi[argmin_rho])
-
-        smoothed_rho = savgol_filter(rho, window_length, 3)
-        peaks = find_peaks(smoothed_rho, height=0)
-
-        # from matplotlib import pyplot as plt
-        # plt.plot(rho)
-        # plt.show()
-        # plt.plot(smoothed_rho)
-        # plt.show()
-        # plt.plot(rho)
-        # for xc in peaks[0]:
-        #     plt.axvline(x=xc)
-        # plt.show()
-
-        four_lowest_rho_peaks_indices = np.argsort(smoothed_rho[peaks[0]])[:4]  # lowest 4
-        four_lowest_rho_peaks_indices = np.sort(four_lowest_rho_peaks_indices)  # sort ascending
-        four_lowest_rho_peaks_indices = peaks[0][four_lowest_rho_peaks_indices]  # contour indices
-        four_lowest_rho_peaks_indices = np.mod(four_lowest_rho_peaks_indices + argmin_rho, x.shape)  # correct back shift
-        four_lowest_rho_peaks_indices = np.sort(four_lowest_rho_peaks_indices)  # sort ascending
-
-        self.corners = x[four_lowest_rho_peaks_indices], y[four_lowest_rho_peaks_indices]
-        self.center = np.mean(self.corners)
-        self.facets = []  # list of Facet objects
-
-        contour_split = np.split(self.contour, four_lowest_rho_peaks_indices)
-        contour_split[0] = np.vstack(contour_split[-1], contour_split[0])
-
-        for i in range(4):
-            facet = Facet(self, )
-
-
-        def retrieveCorners():
-            pass
-
-        def createFacets():
-            pass
-
-    # TODO: Piece.rotatePiece from Functions sheet
-    def rotatePiece(self, angle):
+    def rotate_piece(self, angle):
+        # TODO
         pass
 
 
 def pieces_from_masks(masks, image_path):
     image = cv.imread(image_path)
     pieces = list()
-    for i in masks.shape[-1]:
+    for i in range(masks.shape[-1]):
         pieces.append(Piece(masks[:, :, i], image, i))
     return pieces
+
+
+# piece debugging
+def main():
+    from utils import masks_from_via_region_data
+    import os
+
+    via_region_data_json_path, filename = 'dataset/12pieces/val/via_region_data.json', 'front_white.jpg'
+    masks = masks_from_via_region_data(via_region_data_json_path, filename)
+    image_path = os.path.join(os.path.split(via_region_data_json_path)[0], filename)
+    pieces = pieces_from_masks(masks, image_path)
+
+
+if __name__ == '__main__':
+    main()
