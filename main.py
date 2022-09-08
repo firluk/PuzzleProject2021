f103f71d9fae287247c33f026a404f634d6de22a
V V <firluk@gmail.com>
2022-09-08 09:41:50 +0300

    main extraction

---
+++ main.py
@@ -0,0 +1,15 @@
+from mst_solver import MST_Solver
+from puzzle import segment_to_masks_and_extract_pieces, evaluate_edge_compatibility, sort_and_filter
+
+
+def main():
+    pieces = segment_to_masks_and_extract_pieces('./weights/mask_rcnn_puzzle.h5', './plots/full_downscale.png')
+    cmp, iou, mgc, n_facets, n_pieces, n_side_pieces, n_middle_pieces = evaluate_edge_compatibility(pieces)
+    # sort and filter in descending order
+    edges_by_mgc = sort_and_filter(n_pieces, n_facets, 0, mgc, descending=True)
+    edges_by_iou = sort_and_filter(n_pieces, n_facets, 0, iou, descending=False)
+    edges_by_cmp = sort_and_filter(n_pieces, n_facets, 0, cmp, descending=True)
+    # print_figures_with_weights_to_folder(edges_by_mgc, mgc, pieces, 'mgc')
+    # print_figures_with_weights_to_folder(edges_by_iou, iou, pieces, 'iou')
+    # TODO: user selects edges type
+    solution = MST_Solver(6, edges_by_iou).solveMST()
--- puzzle.py
+++ puzzle.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from facet import Facet
-from mst_solver import MST_Solver
+from main import main
 from piece import Piece, pieces_from_masks, masks_in_scale, image_in_scale
 from puzzle_piece_detector.inference_callable import Inference
 
@@ -170,19 +170,6 @@ def paint_facets_according_to_type(masks, pieces):
         left_width, top_height = left + width, top + height
         masks_with_facets[top:top_height, left:left_width] = img
     return masks_with_facets
-
-
-def main():
-    pieces = segment_to_masks_and_extract_pieces('./weights/mask_rcnn_puzzle.h5', './plots/full_downscale.png')
-    cmp, iou, mgc, n_facets, n_pieces, n_side_pieces, n_middle_pieces = evaluate_edge_compatibility(pieces)
-    # sort and filter in descending order
-    edges_by_mgc = sort_and_filter(n_pieces, n_facets, 0, mgc, descending=True)
-    edges_by_iou = sort_and_filter(n_pieces, n_facets, 0, iou, descending=False)
-    edges_by_cmp = sort_and_filter(n_pieces, n_facets, 0, cmp, descending=True)
-    # print_figures_with_weights_to_folder(edges_by_mgc, mgc, pieces, 'mgc')
-    # print_figures_with_weights_to_folder(edges_by_iou, iou, pieces, 'iou')
-    # TODO: user selects edges type
-    solution = MST_Solver(6, edges_by_iou).solveMST()
 
 
 if __name__ == '__main__':
