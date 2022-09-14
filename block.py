b1c2a59c22f09d9de4a3849b288597691e2f4a53
VadimGolo <vadimgohuni@gmail.com>
2022-09-14 15:47:05 +0300

    block.py, mst_solver.py cleaned for submission

--- block.py
+++ block.py
@@ -1,12 +1,10 @@
 import copy
-
 import numpy as np
 
 
 class cellInBlock:
 
     def __init__(self, piece_ind=None, facet_piece_ind=None):
-
         self.piece_ind = piece_ind
         self.facet_piece_ind = facet_piece_ind
 
@@ -34,36 +32,22 @@ class cellInBlock:
 class Block:
 
     def __init__(self, block_id):
-
         self.block = np.expand_dims(np.array([cellInBlock(block_id, 0)]), axis=1)
         self.id = block_id
-        # self.multi_facet
 
     def rotateBlock(self, angle):
         # 1 - left; 2 - flip; 3 - right
-        # TODO: instead if self.block return block - to avoid mutating the block property
-        # if angle == 1:
-        #     block_tmp = copy.deepcopy(np.rot90(self.block, 1))
         if angle == 1:
             self.block = np.rot90(self.block, 1)
         elif angle == 2:
-            # block_tmp = copy.deepcopy(np.rot90(self.block, 2))
             self.block = np.rot90(self.block, 2)
         elif angle == 3:
-            # block_tmp = copy.deepcopy(np.rot90(self.block, -1))
             self.block = np.rot90(self.block, -1)
-        # for cell in block_tmp.flatten():
-        #     cell.rotateCell(angle)
-        # return block_tmp
         for cell in self.block.flatten():
             if cell is not None:
                 cell.rotateCell(angle)
 
     def findCellInBlock(self, p):
-        # ind = np.where(self.block.piece_ind == p)
-        # return ind[0][0], ind[0][1], self.block.facet_piece_ind
-        # TODO: (future) more efficient search algo
-
         for i in range(self.block.shape[0]):
             for j in range(self.block.shape[1]):
                 if self.block[i][j] is not None and self.block[i][j].piece_ind == p:
@@ -81,9 +65,6 @@ def rotateBlockBeforeJoin(block, fp, fp_up, side):
         block.rotateBlock(2)
     elif abs(fp - fp_up) == 2:
         block.rotateBlock(1 + flip)
-    # else:
-    #     return copy.deepcopy(block)
-    # return block
 
 
 def validateMatch(score_mat, cell_lu, cell_rd, dir):
@@ -120,10 +101,10 @@ def joinBlocks(block_a, block_b, p_a, fp_a, p_b, fp_b, score_mat):
     _, fp_b_up = block_b.findCellInBlock(p_b)
     block_a_rotated = copy.deepcopy(block_a)
     block_b_rotated = copy.deepcopy(block_b)
-    # block_a_rotated = rotateBlockBeforeJoin(block_a_rotated, fp_a, fp_a_up, 1)
-    # block_b_rotated = rotateBlockBeforeJoin(block_b_rotated, fp_b, fp_b_up, 2)
+
     rotateBlockBeforeJoin(block_a_rotated, fp_a, fp_a_up, 1)
     rotateBlockBeforeJoin(block_b_rotated, fp_b, fp_b_up, 2)
+
     p_a_location, _ = block_a_rotated.findCellInBlock(p_a)
     p_b_location, _ = block_b_rotated.findCellInBlock(p_b)
 
@@ -179,7 +160,7 @@ def joinBlocks(block_a, block_b, p_a, fp_a, p_b, fp_b, score_mat):
     # </editor-fold>
 
     joined_block = np.empty(block_a_rotated.block.shape, cellInBlock)
-    # TODO: overwrite block property of the target block object with joined_block
+
     used_edge_stack = []
 
     for i, (row_a, row_b) in enumerate(zip(block_a_rotated.block, block_b_rotated.block)):
@@ -221,6 +202,5 @@ def joinBlocks(block_a, block_b, p_a, fp_a, p_b, fp_b, score_mat):
                 joined_block[i][j] = cell_b
             elif cell_a is not None and cell_b is None:
                 joined_block[i][j] = cell_a
-    # TODO: maybe a global list of used edges so it can be updated with temporary used_edge_stack?
 
     return joined_block, used_edge_stack
--- mst_solver.py
+++ mst_solver.py
@@ -1,22 +1,14 @@
-# Python program for Kruskal's algorithm to find
-# Minimum Spanning Tree of a given connected,
-# undirected and weighted graph
-
-# Class to represent a graph
 import numpy as np
-
 import block as bd
 
 
 class MST_Solver:
 
     def __init__(self, piece_def, edges, score_mat):
-        # TODO: asc or desc order of the final scoring system?
-        # TODO: currently adapted for non-decreasing order
         self.V = piece_def[0]  # No. of puzzle pieces
-        self.n_side = piece_def[1]  # TODO: use for frame calculation
-        self.n_middle = piece_def[2]
-        self.n_corner = 4
+        # self.n_side = piece_def[1]  # for future work
+        # self.n_middle = piece_def[2]    # for future work
+        # self.n_corner = 4   # for future work
         self.graph = edges  # sorted edges by score
         self.score_mat = score_mat
 
@@ -29,23 +21,19 @@ class MST_Solver:
 
     # A function that does union of two sets of x and y
     # (uses union by rank)
-
     def union(self, parent, rank, x, y, i, blocks):
-        # def union(self, parent, rank, x, y):
         xroot = self.find(parent, x)
         yroot = self.find(parent, y)
 
         # Attach smaller rank tree under root of
         # high rank tree (Union by Rank)
-        # print(rank[xroot], xroot, rank[yroot], yroot)
 
         x_block = self.findBlock(blocks, xroot)
         y_block = self.findBlock(blocks, yroot)
+        block = bd.joinBlocks(blocks[x_block], blocks[y_block],
+                              self.graph[i][0], self.graph[i][2], self.graph[i][1], self.graph[i][3],
+                              self.score_mat)
         if rank[xroot] < rank[yroot]:
-            # TODO: fix block indecies!!!
-            block = bd.joinBlocks(blocks[x_block], blocks[y_block],
-                                  self.graph[i][0], self.graph[i][2], self.graph[i][1], self.graph[i][3],
-                                  self.score_mat)
             if block is False:
                 return block
             else:
@@ -53,62 +41,41 @@ class MST_Solver:
                 del blocks[x_block]
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
-            block = bd.joinBlocks(blocks[x_block], blocks[y_block],
-                                  self.graph[i][0], self.graph[i][2], self.graph[i][1], self.graph[i][3],
-                                  self.score_mat)
             if block is False:
                 return block
             else:
                 blocks[x_block].block = block[0]
-                # blocks[y_block].id = xroot
                 del blocks[y_block]
             parent[yroot] = xroot
 
         # If ranks are same, then make one as root
         # and increment its rank by one
         else:
-            block = bd.joinBlocks(blocks[x_block], blocks[y_block],
-                                  self.graph[i][0], self.graph[i][2], self.graph[i][1], self.graph[i][3],
-                                  self.score_mat)
             if block is False:
                 return block
             else:
                 blocks[x_block].block = block[0]
-                # blocks[x_block].id = xroot
                 del blocks[y_block]
             parent[yroot] = xroot
             rank[xroot] += 1
 
     def validateEdge(self, i, validationMat):
-
         while (validationMat[self.graph[i][0]][self.graph[i][2]] != 0) and \
                 (validationMat[self.graph[i][1]][self.graph[i][3]] != 0):
             i = i + 1
         return i, self.graph[i]
 
     def findBlock(self, blocks, ind):
-
         for i, block in enumerate(blocks):
             if block.id == ind:
                 return i
 
-    def print_solution(self, assembled_block):
-        for row in assembled_block:
-            for cell in row:
-                if cell is None:
-                    print("None")
-                else:
-                    print("piece %d facet %d" % (cell.piece_ind, cell.facet_piece_ind))
-
     # The main function to construct MST using Kruskal's algorithm
     def solveMST(self):
-
         # result = []  # This will store the resultant MST
-
         # An index variable, used for sorted edges
         i = 0
-
-        # An index variable, used for result[]
+        # An index variable, used as stop-criterion for the While loop
         e = 0
 
         parent = []
@@ -120,13 +87,10 @@ class MST_Solver:
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
-            blocks.append(bd.Block(node))  # create blocks of single piece each TODO: complete the code
+            blocks.append(bd.Block(node))  # creates blocks of single piece each
 
         # Number of edges to be taken is equal to V-1
-        while e < self.V - 1 and i < self.graph.shape[0]-1:
-
-            # TODO: stop the loop if i == self.graph.shape[0]
-            # Step 2: Pick the smallest edge and increment
+        while e < self.V - 1 and i < self.graph.shape[0] - 1:
             # the index for next iteration
             i, [p1, p2, fp1, fp2] = self.validateEdge(i, validation_mat)  # returns edge with 2 available facets
 
@@ -137,99 +101,10 @@ class MST_Solver:
             # cause cycle, include it in result
             # and increment the index of result
             # for next edge
-            if x != y:
-                # print(parent, rank)
-                if self.union(parent, rank, x, y, i, blocks) is not False:
-                    e = e + 1
-                    # result.append([p1, p2, fp1, fp2])
-                    validation_mat[p1][fp1] = 1
-                    validation_mat[p2][fp2] = 1
+            if x != y and self.union(parent, rank, x, y, i, blocks) is not False:
+                e = e + 1
 
+                validation_mat[p1][fp1] = 1
+                validation_mat[p2][fp2] = 1
             i = i + 1
-        # TODO: return matrix of (p,fp)
-        # self.print_solution(blocks[0].block)
         return blocks
-        # Else discard the edge
-
-        # minimumCost = 0
-        # print("Edges in the constructed MST")
-        # for p1, p2, _, _ in result:
-        #     #     minimumCost += weight
-        #     print("%d -- %d" % (p1, p2))
-
-        # print("Minimum Spanning Tree", minimumCost)
-
-
-# Driver code
-
-# edges = [[0, 1, 2, 2], [0, 2, 2, 0], [2, 3, 1, 3], [1, 2, 3, 0],
-#          [1, 3, 2, 0], [2, 3, 0, 0], [0, 3, 1, 0], [0, 1, 1, 3],
-#          [4, 5, 1, 1], [3, 4, 2, 2]]
-# # [4, 5, 1, 1], [3, 4, 2, 2]
-# cmp = np.load("cmp.npy")
-# g = MST_Solver(6, edges, cmp)
-#
-# # n_facets = 4
-# # n_pieces = 24
-# # n_side_pieces = 12
-# # n_middle_pieces = 8
-#
-# # Function call
-# g.solveMST()
-
-# This code is contributed by Neelam Yadav
-
-###### mocks and driver
-def mock1():
-    #
-    # Minimalistic mock having only 5 edges, all of which are part of the solution
-    #
-    # Facets of piece 'p'
-    #
-    #    0
-    # 1 [p] 3
-    #    2
-    #
-    # Suppose the Puzzle is 2 x 3
-    #
-    # [0] [1]
-    # [2] [3]
-    # [4] [5]
-    #
-    # Test 1:   Edges:
-    #
-    # [0]9[1] ((0,1,3,1),9),
-    #  8   1  ((0,2,2,0),8), ((1,3,2,0),1),
-    # [2]4[3] ((2,3,3,1),4),
-    #  1   7  ((2,4,2,0),7), ((3,5,2,0),1),
-    # [4]6[5] ((4,5,3,1),6),
-    #
-    edges_and_vals = \
-        [
-            ((0, 1, 3, 1), 9),
-            ((0, 2, 2, 0), 8), ((1, 3, 2, 0), 1),
-            ((2, 3, 3, 1), 4),
-            ((2, 4, 2, 0), 7), ((3, 5, 2, 0), 1),
-            ((4, 5, 3, 1), 6)
-        ]
-    n_pieces = 6
-    n_facets = 4
-    n_side = 2
-    n_middle = 0
-
-    score_mat = np.zeros((n_pieces, n_pieces, n_facets, n_facets))
-    for i, (edge, val) in enumerate(edges_and_vals):
-        score_mat[edge] = val
-    edges = [edge for (edge, val) in (sorted(edges_and_vals, key=lambda edge_val: edge_val[1], reverse=True))]
-
-    return (n_pieces, n_side, n_middle), edges, score_mat
-
-
-def main():
-    piece_def, edges, score_mat = mock1()
-    solver = MST_Solver(piece_def, edges, score_mat)
-    solver.solveMST()
-
-
-if __name__ == '__main__':
-    main()
